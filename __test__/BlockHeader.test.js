05933d9ce4654b024c2748df06e9b76d059f7445
ben <me@benhunt.io>
2022-08-26 10:45:08 -0500

    Added testing library jest. Able to coinbase tx block header

---
+++ __test__/BlockHeader.test.js
@@ -0,0 +1,27 @@
+import BlockHeader from '../lib/BlockHeader';
+import reverse from 'buffer-reverse';
+import { decode } from 'punycode';
+
+test('Decode block header with only coinbase transaction', () => {
+
+    let rawHeader = 
+        // Header
+        '00000020' +
+        '06226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f' + 
+        'f19b86227d3b6c7e3786a506ffb4fc6b1942204bd375c4f7fec9d9b900f7f4d8' +
+        'f5fd0463' +
+        'ffff7f20' +
+        '02000000' +
+        // Transactions
+        '01020000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff03510101ffffffff0200f2052a0100000016001452fff61529f49be656745e9b01531167a94762420000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf90120000000000000000000000000000000000000000000000000000000000000000000000000';
+
+    let header = new BlockHeader(rawHeader, 'BigEndian');
+
+    let decodedHeader = header.getBlockHeader();
+    expect(decodedHeader.version).toBe(32);    
+    expect(decodedHeader.previousBlockHash).toBe('06226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f');
+    expect(decodedHeader.merkleRoot).toBe('f19b86227d3b6c7e3786a506ffb4fc6b1942204bd375c4f7fec9d9b900f7f4d8');
+    expect(new Date(decodedHeader.time*1000)).toStrictEqual(new Date('2022-08-23T16:19:01.000Z'));
+    expect(decodedHeader.bits).toBe('207fffff')
+    expect(decodedHeader.nonce).toBe(2);
+})
--- lib/BlockHeader.ts
+++ lib/BlockHeader.ts
@@ -1,27 +1,72 @@
 import { Buffer } from 'buffer';
+import reverse from 'buffer-reverse';
+
+
+type ByteOrder = 'BigEndian' | 'LittleEndian';
 
 export default class BlockHeader {
 
     private rawHexData : string;
 
-    constructor(rawHexData : string){
+    // Header Fields
+    private version : Buffer;
+    private previousBlockHash : Buffer;
+    private merkleRoot : Buffer;
+    private time : Buffer;
+    private bits : Buffer;
+    private nonce : Buffer;
+
+
+    /**
+     * Hex encoded blocks returned from 'getblocks' is mostly given to us in BigEndian except
+     * for the fields: {time, nonce} which are in LittleEndian format
+     */
+    constructor(rawHexData : string, byteOrder : ByteOrder){
         this.rawHexData = rawHexData;
 
         // @ts-ignore
         let reader = new BYOBReader(rawHexData, 'hex');
 
-        let result = reader.read(Buffer.alloc(1));
+        // Store the bytes in the same order as blocks are encoded and sent to the blockchain.
+        if(byteOrder == 'LittleEndian'){
+            this.version = reader.read(Buffer.alloc(4));
+            this.previousBlockHash = reader.read(Buffer.alloc(32));
+            this.merkleRoot = reader.read(Buffer.alloc(32));
+            this.time = reader.read(Buffer.alloc(4));
+            this.bits = reader.read(Buffer.alloc(4));
+            this.nonce = reader.read(Buffer.alloc(4));
+        }
+        else {
+            this.version = reverse(reader.read(Buffer.alloc(4)));
+            this.previousBlockHash = reverse(reader.read(Buffer.alloc(32)));
+            this.merkleRoot = reverse(reader.read(Buffer.alloc(32)));
+            this.time = reader.read(Buffer.alloc(4));
+            this.bits = reverse(reader.read(Buffer.alloc(4)));
+            this.nonce = reader.read(Buffer.alloc(4));
+        }
+    }
 
-        console.log(result);
+    public getBlockHeader(){
+        return {
+            version : this.version.readUint32LE(),
+            previousBlockHash : reverse(this.previousBlockHash).toString('hex'),
+            merkleRoot : reverse(this.merkleRoot).toString('hex'),
+            time : this.time.readUInt32LE(),
+            bits : this.bits.toString('hex'),
+            nonce : this.nonce.readUInt32LE(),
+        }
     }
 
 }
 
 
 
+// Reads bytes from string input. Can select what encoding the string is in that is supported
+// by the Buffer API.
 class BYOBReader {
 
     buffer : Buffer;
+
 
     constructor(data : string, encoding : BufferEncoding){
         this.buffer = Buffer.from(data, encoding);
@@ -29,7 +74,8 @@ class BYOBReader {
 
     read(buffer : Buffer) : Buffer {
 
-        this.buffer.copy(buffer, 0, 0, 2);
+        this.buffer.copy(buffer, 0, 0, buffer.length);
+        this.buffer = this.buffer.subarray(buffer.length, this.buffer.length);
 
         return buffer;
         
