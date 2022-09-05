cccd67b52ab26773a2f23b401e2aec6fc02d4689
ben <me@benhunt.io>
2022-09-05 11:19:43 -0500

    Created collapsable table with dummy data

---
+++ components/BlankHeader.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+class BlankHeader extends React.Component<any, any> {
+
+    constructor(props : any){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div style={{height: "100px"}}/>
+        );
+    }
+}
+
+export default BlankHeader;
